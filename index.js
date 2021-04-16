01a019f29b478f1c229730b52d86a33f0e6fd282
Shreyas Amargol <shreyasamargol@Shreyas-MacBook-Pro.local>
2021-04-16 00:22:30 -0500

    initla commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const secret = "your_secret_here";
+const repo = "~/your_repo_path_here/";
+
+var express = require('express')
+const app = express()
+const port = 4567
+
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded());
+
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+
+// Access the parse results as request.body
+app.post('/', function(req, res){
+    console.log(req.body)
+
+    res.end()
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+
+})
